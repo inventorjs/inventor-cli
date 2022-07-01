c68c8a3b51ef54a3ec6c671132d08f5ed87c964c
sunkeysun <sunkeysun@tencent.com>
2022-07-01 12:33:27 +0800

    feat: 初始化项目结构

---
+++ packages/cli-core/src/index.ts
@@ -0,0 +1,7 @@
+export * as fs from './modules/fs';
+export * as cmd from './modules/cmd';
+export * as env from './modules/env';
+export * as inquire from './modules/inquire';
+export * as pkg from './modules/pkg';
+export * as storage from './modules/storage';
+export * as user from './modules/user';
