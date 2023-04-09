63e35c35aee56f02458ac3e88b85b76c1f05838c
sunkeysun <sunkeysun@outlook.com>
2023-04-09 23:12:35 +0800

    feat: 添加api-service

--- packages/sls-core/src/index.ts
+++ packages/sls-core/src/index.ts
@@ -2,53 +2,32 @@
  * serverless core
  */
 import { resolveSlsTemplate } from './util.js'
-// import ServerlessUtils from '@serverless/utils-china'
+import { ApiService } from './api-service.js'
+
+const apiService = new ApiService({
+  appId: '',
+  secretId: '',
+  secretKey: '',
+})
 
 async function run() {
-  const template = await resolveSlsTemplate(
-    '/Users/sunkeysun/Projects/Other/cli/packages/sls-core/.serverless',
-  )
+  const template = await resolveSlsTemplate('')
 
   console.log(JSON.stringify(template, null, 2))
+
+  // const data = await apiService.getCacheFileUrls({
+  //   appName: 'scf-starter',
+  //   stageName: 'dev',
+  //   instanceName: 'scfdemo-1',
+  // })
+  if (!template) return
+
+  const data = await apiService.runComponent({
+    instance: template.instances[0],
+    method: 'deploy',
+  })
+
+  console.log(data)
 }
 
 run()
-
-// const { Serverless } = ServerlessUtils
-
-// export class Sls {
-//   private sdk: Serverless
-
-//   constructor(private readonly slsPath: string) {
-//     const {
-//       TENCENT_APP_ID: appid = '',
-//       TENCENT_SECRET_ID: secretId = '',
-//       TENCENT_SECRET_KEY: secretKey = '',
-//       TENCENT_TOKEN: token = '',
-//       SERVERLESS_PLATFORM_STAGE: stage,
-//     } = process.env
-//     const region = getStageRegion(stage)
-//     this.sdk = new Serverless({
-//       appid,
-//       secretId,
-//       secretKey,
-//       options: {
-//         region,
-//         token,
-//         sdkAgent: '@inventorjs/sls-core',
-//         traceId: ''
-//       }
-//     })
-//   }
-
-//   async dev() { }
-//   async deploy() {
-//     const template = await resolveSlsTemplate(this.slsPath)
-//     if (!template) {
-//       console.log('没有实例')
-//     }
-
-//   }
-//   async remove() { }
-//   async info() { }
-// }
